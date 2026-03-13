4b28a0cf02868701c208836a31675fab258f0ac3
kevindotklein <kevindotklein@gmail.com>
2026-03-12 22:00:58 -0300

    chore: remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Carousel from "./components/custom/carousel";
 import Header from "./components/custom/header";
 import AboutUs from "./components/custom/about-us";
-import { Badge, Container, Flex, Text } from "@radix-ui/themes";
-import { ArrowRightIcon, CheckIcon, HomeIcon, RulerHorizontalIcon } from "@radix-ui/react-icons";
 import Services from "./components/custom/services";
 
 export default function Home() {
